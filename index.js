90456229b623b3a4f1995133792c2867110e304e
Anna Baldwin <annabaldwin56@gmail.com>
2022-01-09 20:41:55 +0100

    completed lab

--- index.js
+++ index.js
@@ -15,9 +15,45 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  //Iteration 3
+  Recipe.insertMany(data)
+  .then(recipe => {
+    recipe.forEach(recipe => console.log(recipe.title));
+    modifyRecipe();
+    deleteRecipe()
+    })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  
+  //Iteration 4
+  async function modifyRecipe() {
+    try {
+      const query = { title: 'Rigatoni alla Genovese' };
+      const toUpdate = await Recipe.findOneAndUpdate(
+        query, { duration: 100 }, { new: true }
+        );
+      console.log('Updated: ', toUpdate.title, 'to duration: ', toUpdate.duration);
+    } catch (err) {
+      console.error(err);
+      process.exit();
+    }
+  }
+
+  //Iteration 5
+  async function deleteRecipe() {
+    try {
+      const deleteRec = await Recipe.deleteOne(
+      { title: 'Carrot Cake' }
+      ); 
+      console.log('deleted? ', deleteRec);
+    } catch (err) {
+      console.error(err);
+      process.exit()
+    }
+  }
